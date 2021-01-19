63a985ab04055343fba01d0c65e221846d1622b6
unknown <szeszej@gmail.com>
2021-01-19 11:08:44 +0100

    small fixes

--- src/components/skills-calculator/skill-line/skill-tooltip/SkillTooltip.tsx
+++ src/components/skills-calculator/skill-line/skill-tooltip/SkillTooltip.tsx
@@ -3,26 +3,25 @@ import React from "react";
 
 //Types
 import { Skill } from "../../../../data/models/skill.model";
-import { TransitionStatus } from 'react-transition-group/Transition';
+import { TransitionStatus } from "react-transition-group/Transition";
 
 export class SkillTooltip extends React.Component<PropTypes> {
-
   render() {
     const transitionStyles: { [id: string]: React.CSSProperties } = {
       entering: { maxHeight: "40em" },
-      entered:  { maxHeight: "40em" },
-      exiting:  { maxHeight: 0 },
-      exited:  { maxHeight: 0 },
+      entered: { maxHeight: "40em" },
+      exiting: { maxHeight: 0 },
+      exited: { maxHeight: 0 },
     };
-    console.log(this.props.state);
-
-    return (<div className="skillTooltip" style={transitionStyles[this.props.state]}>
-      <p>{this.props.skill.description}</p>
-    </div>)
+    return (
+      <div className="skillTooltip" style={transitionStyles[this.props.state]}>
+        <p>{this.props.skill.description}</p>
+      </div>
+    );
   }
 }
 
 interface PropTypes {
-  skill: Skill
-  state: TransitionStatus
+  skill: Skill;
+  state: TransitionStatus;
 }
