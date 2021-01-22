a89e6295796ee44ef260b6e9729c751cc9332bb9
unknown <szeszej@gmail.com>
2021-01-22 12:51:19 +0100

    deleted some text for easier troubleshooting

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -10,32 +10,6 @@ function ConnectedHome(props: PropTypes) {
     <div className="home">
       <div className="intro">
         <h2>Taern je fajny</h2>
-        <h2>{t("intro-header-1")}</h2>
-        <p>{t("intro-desc-1")}</p>
-        <h2>{t("intro-header-2")}</h2>
-        <p>{t("intro-desc-2")}</p>
-        <h2>{t("intro-header-3")}</h2>
-        <p>{t("intro-desc-3-1")}</p>
-        <ol>
-          <li>{t("intro-desc-3-bullet-1")}</li>
-          <li>{t("intro-desc-3-bullet-2")}</li>
-          <li>{t("intro-desc-3-bullet-3")}</li>
-        </ol>
-        <p>{t("intro-desc-3-2")}</p>
-        <h2>{t("intro-header-4")}</h2>
-        <p>
-          {t("intro-desc-4-1")} (
-          <img className="imageInText" src="images/upgrade.png" alt="upgrade" />
-          ). {t("intro-desc-4-2")}
-        </p>
-        <h2>
-          {t("intro-header-5")} (
-          <img className="imageInText" src="images/star.svg" alt="kowadło" />
-          )?
-        </h2>
-        <p>{t("intro-desc-5")}</p>
-        <h2>{t("intro-header-6")}</h2>
-        <p>{t("intro-desc-6")}</p>
       </div>
     </div>
   );
